7a0bf79cb9938124af53cd51450a2c3308637eca
Nikita Kolmogorov <backmeupplz@gmail.com>
2024-04-24 15:42:22 -0700

    turn off tests for now

--- test/LunchbreakSeats.ts
+++ test/LunchbreakSeats.ts
@@ -1,24 +1,23 @@
-import { ethers } from 'hardhat'
-import { expect } from 'chai'
+// import { ethers } from 'hardhat'
+// import { expect } from 'chai'
 
 describe('LunchbreakSeats contract tests', () => {
-  before(async function () {
-    const accounts = await ethers.getSigners()
-    this.owner = accounts[0]
-    this.factory = await ethers.getContractFactory('MyERC721')
-  })
-
-  describe('Constructor', function () {
-    it('should deploy the contract with the correct fields', async function () {
-      const name = 'MyERC721'
-      const symbol = 'TOKEN'
-      const contract = await this.factory.deploy(
-        name,
-        symbol,
-        this.owner.address
-      )
-      expect(await contract.name()).to.equal(name)
-      expect(await contract.symbol()).to.equal(symbol)
-    })
-  })
+  // before(async function () {
+  //   const accounts = await ethers.getSigners()
+  //   this.owner = accounts[0]
+  //   this.factory = await ethers.getContractFactory('MyERC721')
+  // })
+  // describe('Constructor', function () {
+  //   it('should deploy the contract with the correct fields', async function () {
+  //     const name = 'MyERC721'
+  //     const symbol = 'TOKEN'
+  //     const contract = await this.factory.deploy(
+  //       name,
+  //       symbol,
+  //       this.owner.address
+  //     )
+  //     expect(await contract.name()).to.equal(name)
+  //     expect(await contract.symbol()).to.equal(symbol)
+  //   })
+  // })
 })
