a799233d91f125c108bb996c15613e1f4d9e8c91
Nikita Kolmogorov <backmeupplz@gmail.com>
2024-04-25 09:07:10 -0700

    finish tests for utility functions

--- test/LunchbreakSeats.ts
+++ test/LunchbreakSeats.ts
@@ -28,4 +28,98 @@ describe('LunchbreakSeats contract tests', () => {
       )
     })
   })
+
+  describe('Setters', function () {
+    describe('Set Fee Recipient', function () {
+      const newFeeRecipient = '0x000000000000000000000000000000000000dEaD'
+
+      it('should allow the owner to set a new fee recipient', async function () {
+        await lunchbreakSeats.setFeeRecipient(newFeeRecipient)
+        expect(await lunchbreakSeats.feeRecipient()).to.equal(newFeeRecipient)
+      })
+
+      it('should not allow non-owner to set a new fee recipient', async function () {
+        const [, nonOwner] = await ethers.getSigners()
+        await expect(
+          lunchbreakSeats.connect(nonOwner).setFeeRecipient(newFeeRecipient)
+        ).to.be.revertedWithCustomError(
+          lunchbreakSeats,
+          'OwnableUnauthorizedAccount'
+        )
+      })
+    })
+
+    describe('Set Fee Divider', function () {
+      const newFeeDivider = 25 // Changing the fee divider for example purposes
+
+      it('should allow the owner to set a new fee divider', async function () {
+        await lunchbreakSeats.setFeeDivider(newFeeDivider)
+        expect(await lunchbreakSeats.feeDivider()).to.equal(newFeeDivider)
+      })
+
+      it('should not allow non-owner to set a new fee divider', async function () {
+        const [, nonOwner] = await ethers.getSigners()
+        await expect(
+          lunchbreakSeats.connect(nonOwner).setFeeDivider(newFeeDivider)
+        ).to.be.revertedWithCustomError(
+          lunchbreakSeats,
+          'OwnableUnauthorizedAccount'
+        )
+      })
+    })
+
+    describe('Set Compensation Divider', function () {
+      const newCompensationDivider = 15 // Example new value
+
+      it('should allow the owner to set a new compensation divider', async function () {
+        await lunchbreakSeats.setCompensationDivider(newCompensationDivider)
+        expect(await lunchbreakSeats.compensationDivider()).to.equal(
+          newCompensationDivider
+        )
+      })
+
+      it('should not allow non-owner to set a new compensation divider', async function () {
+        const [, nonOwner] = await ethers.getSigners()
+        await expect(
+          lunchbreakSeats
+            .connect(nonOwner)
+            .setCompensationDivider(newCompensationDivider)
+        ).to.be.revertedWithCustomError(
+          lunchbreakSeats,
+          'OwnableUnauthorizedAccount'
+        )
+      })
+    })
+  })
+
+  describe('Ownership Transfer', function () {
+    let newOwner
+
+    before(async function () {
+      ;[, newOwner] = await ethers.getSigners()
+    })
+
+    it('should transfer ownership successfully', async function () {
+      await lunchbreakSeats.transferOwnership(newOwner.address)
+      expect(await lunchbreakSeats.owner()).to.equal(newOwner.address)
+    })
+
+    it('new owner should be able to set fee divider', async function () {
+      const newFeeDivider = 25
+      await expect(
+        lunchbreakSeats.connect(newOwner).setFeeDivider(newFeeDivider)
+      ).not.to.be.reverted
+      expect(await lunchbreakSeats.feeDivider()).to.equal(newFeeDivider)
+    })
+
+    it('previous owner should not have administrative rights', async function () {
+      const newFeeDivider = 30
+      await expect(
+        lunchbreakSeats.setFeeDivider(newFeeDivider)
+      ).to.be.revertedWithCustomError(
+        lunchbreakSeats,
+        'OwnableUnauthorizedAccount'
+      )
+    })
+  })
 })
