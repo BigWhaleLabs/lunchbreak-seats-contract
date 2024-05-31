92698f7019384c18a0207e53bd45e4d10446803d
Nikita Kolmogorov <backmeupplz@gmail.com>
2024-05-31 14:50:31 -0700

    add verification to upgrade scrip

--- scripts/upgrade.ts
+++ scripts/upgrade.ts
@@ -1,4 +1,4 @@
-import { ethers, upgrades } from 'hardhat'
+import { ethers, run, upgrades } from 'hardhat'
 import prompt from 'prompt'
 
 const ethAddressRegex = /^0x[a-fA-F0-9]{40}$/
@@ -29,6 +29,25 @@ async function main() {
     await upgrades.erc1967.getAdminAddress(await contract.getAddress()),
     ' getAdminAddress'
   )
+  console.log('Wait for 1 minute to make sure blockchain is updated')
+  await new Promise((resolve) => setTimeout(resolve, 60 * 1000))
+  // Try to verify the contract on Etherscan
+  console.log('Verifying contract on Etherscan')
+  try {
+    await run('verify:verify', {
+      address: await upgrades.erc1967.getImplementationAddress(
+        await contract.getAddress()
+      ),
+      constructorArguments: [],
+    })
+  } catch (err) {
+    console.log(
+      'Error verifiying contract on Etherscan:',
+      err instanceof Error ? err.message : err
+    )
+  }
+  // Print out the information
+  console.log(`Done!`)
 }
 
 main().catch((error) => {
